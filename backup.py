bea6e80c170c2fc93cf52a8a90aceb9c9a7b318b
achhunna <achhunna@gmail.com>
2015-06-19 16:20:57 -0700

    Added main function

--- backup.py
+++ backup.py
@@ -1,5 +1,4 @@
 ''' Script to copy file into Backup/ folder with timestamp on filename (C) 2015 Achhunna Mali'''
-# Hosted on GitHub https://github.com/achhunna/Backup
 
 import os
 import shutil
@@ -9,9 +8,12 @@ from datetime import datetime
 timeStamp = "%b%d%Y_%I%M%S %p"
 timeStampStr = datetime.now().strftime(timeStamp)
 
-
 # Define network location path
 locationPath = "//rws-filer1/GMO_finance/Database/"
+
+# Declare filename and folder variable
+fullFileName = "Feeder.xlsx"
+folder = "Backup"
 
 # Define file copying function
 def copyFile(old_file_name, new_file_name, folderName):
@@ -23,26 +25,26 @@ def copyFile(old_file_name, new_file_name, folderName):
         dst_file = os.path.join(dst_dir, old_file_name)
         new_dst_file_name = os.path.join(dst_dir, new_file_name)
         os.rename(dst_file, new_dst_file_name)
-		
-# Declare filename and folder variable
-fullFileName = "Feeder.xlsx"
-folder = "Backup"
 
-# Split extension
-periodLoc = fullFileName.find(".")
-fileName = fullFileName[:periodLoc]
-extension = fullFileName[periodLoc:]
+# Define main function		
+def main():
+	# Split extension
+	periodLoc = fullFileName.find(".")
+	fileName = fullFileName[:periodLoc]
+	extension = fullFileName[periodLoc:]
 
-# Create timestamp filename
-timeStampName = fileName + "_" + timeStampStr + extension
+	# Create timestamp filename
+	timeStampName = fileName + "_" + timeStampStr + extension
 
-# Change directory
-os.chdir(locationPath)
+	# Change directory
+	os.chdir(locationPath)
 
-# Copy timestamp file into folder
-try:
-		copyFile(fullFileName, timeStampName, folder)
-		print("Done!")
-except Err:
-		print("Error:" + Err)
+	# Copy timestamp file into folder
+	try:
+			copyFile(fullFileName, timeStampName, folder)
+			print("Done!")
+	except Err:
+			print("Error:" + Err)
 
+if __name__ == "__main__":
+	main()
